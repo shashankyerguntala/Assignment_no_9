44df3edc847e8dc875096f71dff04b421aad2299
shashankyerguntala <shashankyerguntala27@gmail.com>
2025-09-15 10:33:58 +0530

    changes

--- lib/presentation/features/all_chats/bloc/all_chats_event.dart
+++ lib/presentation/features/all_chats/bloc/all_chats_event.dart
@@ -1,7 +1,5 @@
 import 'package:equatable/equatable.dart';
-import 'package:assignment_9/domain/entity/message_entity.dart'; 
-
-
+import 'package:assignment_9/domain/entity/message_entity.dart';
 
 abstract class AllChatsEvent extends Equatable {
   const AllChatsEvent();
@@ -10,7 +8,6 @@ abstract class AllChatsEvent extends Equatable {
   List<Object?> get props => [];
 }
 
-/// Event dispatched from the UI to start loading the chat streams.
 class LoadAllChats extends AllChatsEvent {
   final String currentUserId;
 
@@ -20,7 +17,6 @@ class LoadAllChats extends AllChatsEvent {
   List<Object?> get props => [currentUserId];
 }
 
-/// Internal event used by the BLoC to process new data from the stream.
 class ChatsUpdated extends AllChatsEvent {
   final List<MessageEntity> messages;
 
